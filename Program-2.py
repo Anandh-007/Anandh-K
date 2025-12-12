929aadb947507838843c11487c7bb758bf3b2c57
Anandh-007 <113185402+Anandh-007@users.noreply.github.com>
2025-12-12 22:20:59 +0530

    Implement series_problem_2 function in Program-2.py

---
+++ Program-2.py
@@ -0,0 +1,10 @@
+def series_problem_2(a: int):
+    result = []
+    for i in range(1, a + 1):
+        result.append(2 * i - 1)
+    return result
+
+print(series_problem_2(1))  
+print(series_problem_2(2)) 
+print(series_problem_2(3))  
+print(series_problem_2(4))  
